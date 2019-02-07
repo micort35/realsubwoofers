447036593dbb6bba298f6876532a4d5ba97b4057
Michael Ortiz <micort35@gmail.com>
2019-02-07 10:43:05 -0500

    Formatting changes

--- realsubwoofers.py
+++ realsubwoofers.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-from twython import Twython
 import random
 import os
 import string
+from twython import Twython
 from logzero import logger
 
 #get necessary keys and interface with Twitter API
@@ -17,25 +17,27 @@ def create_client():
 def tweet():
     #establish interface with Twitter API
     twitter = create_client()
-    #get random follow's handle to format message with
+
+    #get random follower's handle and message
     followers = (twitter.get_followers_ids()['ids'])
     follower = random.choice(followers)
     user = twitter.lookup_user(user_id = follower)[0]
     id = user['screen_name']
     messages = open('./assets/messages.txt').read().splitlines()
-    message = random.choice(messages)
-    message = message.format(id)
-    #get random file from library to pair with message
+    message = random.choice(messages).format(id)
+
+    #get random media from library to pair with message
     images = './assets/media/'
     image = random.choice(os.listdir(images))
     image_path = images + image
-    #attempt to tweet, log variables and attempt outcome
-    logger.info('Attempting to tweet the following: message = %s, image = %s'
-            , message, image_path)
+
+    #attempt to tweet, log variables and the attempt outcome
+    logger.info('Attempting to tweet the following: message = %s, image = %s',
+            message, image_path)
     with open(image_path, 'rb') as photo:
         response = twitter.upload_media(media=photo)
-        twitter.update_status(status = message
-                            , media_ids = [response['media_id']])
+        twitter.update_status(status = message,
+                            media_ids = [response['media_id']])
     logger.info('API response: %s', tweet)
 
 def lambda_handler(event, context):
